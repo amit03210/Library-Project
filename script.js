d202255212adac9a3dcee7582ed9803ce0965e6c
amit03210 <ami9cse@gmail.com>
2025-08-09 17:29:47 +0530

    added readme

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-class Book{
-    constructor(title, author, pageRead, totalPage, defaultBook = false, readonly = false){
+class Book {
+    constructor(title, author, pageRead, totalPage, defaultBook = false, readonly = false) {
         this.title = title;
         this.author = author;
         this.pageRead = pageRead;
@@ -9,7 +9,6 @@ class Book{
         this.defaultBook = defaultBook;
     }
 }
-
 
 let addNewBook = document.querySelector('.add-new');
 let tbody = document.querySelector('.table-body');
@@ -21,10 +20,10 @@ displayDefaultBook();
 
 //event
 addNewBook.addEventListener('click', addBookToLibrary);
-tbody.addEventListener('click', (e) =>{
-    if(e.target.classList.contains('edit-book'))
+tbody.addEventListener('click', (e) => {
+    if (e.target.classList.contains('edit-book'))
         editBook(e);
-    if(e.target.classList.contains('del-book')){
+    if (e.target.classList.contains('del-book')) {
         delRow(e);
     }
 });
@@ -32,17 +31,17 @@ search.addEventListener('keyup', searchFunction);
 
 //method
 
-function displayDefaultBook(){
+function displayDefaultBook() {
     const newBook1 = new Book('Harry Potter', 'JK Rowling', 2000, 3000, true, true);
     const newBook2 = new Book('Wednesday', 'Tehlor Kay Mejia', 521, 1521, true, true);
     const newBook3 = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 65, 206, true, true);
     const newBook4 = new Book('Pride and Prejudice', 'Jane Austen', 25, 184, true, true);
     myLibrary.push(newBook1, newBook2, newBook3, newBook4);
     renderUI();
-    
+
 }
-function addBookToLibrary(){
-    const newBook = new Book("", "", "", "" );
+function addBookToLibrary() {
+    const newBook = new Book("", "", "", "");
     myLibrary.push(newBook);
     renderUI();
 
@@ -79,10 +78,10 @@ function renderUI() {
         inputs.forEach(input => {
             progressBar.max = book.totalPage;
             progressBar.value = book.pageRead;
-            
+
             input.readOnly = book.readOnly;
             input.classList.toggle('addInputBorder', !book.readOnly);
-       
+
         })
 
         //styling button:
@@ -91,7 +90,7 @@ function renderUI() {
 
 }
 
-function editBook(e){
+function editBook(e) {
 
     const button = e.target;
     //learn something new
@@ -104,13 +103,13 @@ function editBook(e){
     const rowTotalPage = row.querySelector('.row-page');
     const rowProgressBar = row.querySelector('.progress-bar');
 
-    
+
 
     myLibrary.forEach(book => {
-        if(book.id === bookID){
+        if (book.id === bookID) {
             //updating data after edit
-            book.title = rowTitle.value === "" ?"" : rowTitle.value;           
-            book.author = rowAuthor.value === "" ?"" : rowAuthor.value;
+            book.title = rowTitle.value === "" ? "" : rowTitle.value;
+            book.author = rowAuthor.value === "" ? "" : rowAuthor.value;
             book.pageRead = rowPageRead.value;
             book.totalPage = rowTotalPage.value;
             rowProgressBar.max = book.totalPage;
@@ -118,15 +117,15 @@ function editBook(e){
 
             //notifying title and author should not be empty else send alert.
             //if both value present than toggle readonly else reportValidity();
-            if(book.title && book.author){
-                book.readOnly = book.readOnly?false:true;
+            if (book.title && book.author) {
+                book.readOnly = book.readOnly ? false : true;
                 inn.forEach(input => {
                     //add class when in edit mode and update readonly attribute.
                     input.readOnly = book.readOnly;
                     input.classList.toggle('addInputBorder', !book.readOnly);
                     changeButtonBG(e.target, book);
-                })   
-            }else{
+                })
+            } else {
                 rowAuthor.reportValidity();
                 rowTitle.reportValidity();
             }
@@ -135,57 +134,57 @@ function editBook(e){
     styleRow(rowProgressBar, inn);
 }
 
- function changeButtonBG(editBtn, obj){
-        if(!obj.readOnly){
-            editBtn.style.backgroundColor = '#4CAF50';
-            editBtn.style.color = 'white';
-        }else{
-            editBtn.style.backgroundColor = "";
-            editBtn.style.color = "";
-        }
- }
+function changeButtonBG(editBtn, obj) {
+    if (!obj.readOnly) {
+        editBtn.style.backgroundColor = '#4CAF50';
+        editBtn.style.color = 'white';
+    } else {
+        editBtn.style.backgroundColor = "";
+        editBtn.style.color = "";
+    }
+}
 
- function delRow(e){
+function delRow(e) {
     const trow = e.target.closest('tr');
     console.log(e)
     myLibrary.forEach(book => {
-        if(book.id === trow.id){
+        if (book.id === trow.id) {
             trow.remove();
         }
     })
     myLibrary = myLibrary.filter(book => book.id !== trow.id);
     renderUI();
- }
-
-function searchFunction(e){
- let searchTerm = search.value.toLowerCase();
- let body = e.target.closest('body');
- let trows = body.querySelectorAll('tbody tr');
-
-if(searchTerm == ""){
-    trows.forEach(row => row.classList.remove('hidden'));
-    return;
 }
-//  console.log(trows)
- trows.forEach(row => {
-    const book = myLibrary.find(b => b.id === row.id);
-    if (book && (book.title.toLowerCase().includes(searchTerm) || book.author.toLowerCase().includes(searchTerm))) {
-      row.classList.remove('hidden');
-    } else {
-      row.classList.add('hidden');
+
+function searchFunction(e) {
+    let searchTerm = search.value.toLowerCase();
+    let body = e.target.closest('body');
+    let trows = body.querySelectorAll('tbody tr');
+
+    if (searchTerm == "") {
+        trows.forEach(row => row.classList.remove('hidden'));
+        return;
     }
-  });
+    //  console.log(trows)
+    trows.forEach(row => {
+        const book = myLibrary.find(b => b.id === row.id);
+        if (book && (book.title.toLowerCase().includes(searchTerm) || book.author.toLowerCase().includes(searchTerm))) {
+            row.classList.remove('hidden');
+        } else {
+            row.classList.add('hidden');
+        }
+    });
 
 }
 
-function styleRow(progressElement, inn){
+function styleRow(progressElement, inn) {
     // console.log(progressElement)
-    
-    if(parseFloat(progressElement.max) === parseFloat(progressElement.value)){
-            // console.log("hi");
-            inn.forEach(input => input.style.textDecoration='line-through')
-        }else{
-            inn.forEach(input => input.style.textDecoration = 'none');
-        }
+
+    if (parseFloat(progressElement.max) === parseFloat(progressElement.value)) {
+        // console.log("hi");
+        inn.forEach(input => input.style.textDecoration = 'line-through')
+    } else {
+        inn.forEach(input => input.style.textDecoration = 'none');
+    }
 }
 
